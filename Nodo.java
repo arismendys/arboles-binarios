b3424e43b4258f98994d7efa50227fd78e716420
Arismendys Marte <arismendys@gmail.com>
2018-12-07 15:18:43 -0400

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,6 +1,13 @@
 /*
  Esta clase nos permitira crear nuestros Nodos en las deferentes clases que necesitemos.
  
+* El metodo setData recibe un dato tipo Object el cual se almacenara en el nodo
+* El metodo setNext recibe el nodo que ira despues del nodo actual
+* El metodo setBefore recibe el nodo antorior al que apuntara el actual
+* El metodo getData retorna un tipo Object con los datos contenidos en un nodo
+* El metodo getBefore retorna el nodo anterior al nodo actual
+* El metodo getNext retorna al nodo en la posicion siguiente al nodo actual 
+ 
  Para utilizar en otras clases recomiendo cambiar setData y getData a Object, la utilizaremos como int para trabajar
  nuestro Arbol Binario.
  
